9733164cb5d87a998a09a61c34c6c3fe36849431
Victor <vagprogrammer@gmail.com>
2016-10-28 15:04:04 -0500

    Select pokemons to transfer is working

--- mobile/src/main/java/com/javic/pokewhere/adapters/AdapterPokemonBank.java
+++ mobile/src/main/java/com/javic/pokewhere/adapters/AdapterPokemonBank.java
@@ -39,7 +39,7 @@ public class AdapterPokemonBank extends DragSelectRecyclerViewAdapter<AdapterPok
 
     @Override
     public PokemonBankViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_pokemon, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_item_pokemon, parent, false);
         return new PokemonBankViewHolder(v);
     }
 
